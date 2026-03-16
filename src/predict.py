c99e5654d97473ec52a6bf811db66a6c43d80f94
zerohyeon <joyh0808@gmail.com>
2026-03-16 16:04:19 +0900

    feat: 테스트 데이터 기반 최종 예측 결과 생성 및 저장

--- src/predict.py
+++ src/predict.py
@@ -46,11 +46,11 @@ def main():
 
     predictions = model.predict(processed_test)
 
-    # 6. 제출 양식(sample_submission)의 타겟 컬럼에 예측값 덮어씌우기
+    # 제출 양식(sample_submission)의 타겟 컬럼에 예측값 덮어씌우기
     target_col = config['features']['target']
     submission_df[target_col] = predictions
     
-    # 7. 현재 시간으로 다이내믹한 파일명 생성 및 저장
+    # 현재 시간으로 다이내믹한 파일명 생성 및 저장
     os.makedirs(save_dir, exist_ok=True) # submissions 폴더가 없으면 알아서 만듦
     
     # datetime.now()를 포맷팅하여 "년월일시분" 문자열 생성 (예: 202603161315)
@@ -58,13 +58,12 @@ def main():
     final_filename = f"submission_{current_time}.csv"
     final_filepath = os.path.join(save_dir, final_filename)
     
-    # index=False 로 설정해야 파일 맨 왼쪽에 쓸데없는 0, 1, 2, 3 번호가 안 붙습니다.
+    # index=False 로 설정해야 파일 맨 왼쪽에 쓸데없는 0, 1, 2, 3 번호가 붙지 않음.
     submission_df.to_csv(final_filepath, index=False)
     
     print("\n========================================")
-    print(f"🎉 모든 과정 완료! 최종 제출 파일이 생성되었습니다.")
-    print(f"📂 저장 위치: {final_filepath}")
-    print("========================================\n")
+    print("최종 제출 파일이 생성 완료")
+    print("\n========================================")
 
 if __name__ == "__main__":
     main()
